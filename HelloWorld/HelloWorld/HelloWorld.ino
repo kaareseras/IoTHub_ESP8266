6c9c58a5df87951eaa2c78d374c3d67a854747d8
Kåre Rasmussen <kaare@seras.dk>
2019-12-03 21:32:36 +0100

    update readme IoTHub

---
+++ HelloWorld/HelloWorld/HelloWorld.ino
@@ -0,0 +1,16 @@
+#define LED_BUILTIN 2
+
+// the setup rutine thatonly run once, before entering the loop
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+                                    // but actually the LED is on; this is because 
+                                    // it is acive low on the ESP-01)
+  delay(1000);                      // Wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+  delay(2000);                      // Wait for two seconds (to demonstrate the active low LED)
+}
